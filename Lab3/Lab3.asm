0ca5480c373d31d6979a4ca5f9b7f48a8a2f66ca
Kameron Gill <kgill2@Ucsc.edu>
2017-02-14 15:54:36 -0800

    started lab3

---
+++ Lab3/Lab3.asm
@@ -0,0 +1,14 @@
+;Kameron GIll
+;1467833
+;kgill2@ucsc.edu
+;Lab Section: TuThurs 7-9PM
+
+;Lab 3
+;Decimal to binary 2-bits compliments converter
+
+.ORIG x3000
+
+LEA R0, GREETINGS	;PRINTS GREETINGS MESSAGE ON STARTUP
+PUTS			;PRINTS GREETINGS
+
+
